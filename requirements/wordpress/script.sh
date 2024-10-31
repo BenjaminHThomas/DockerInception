2fec242e9daedb9b10499eb5cf7bb65274c6e7ff
ben <benjamin.h.thomas1@gmail.com>
2024-10-31 17:12:45 +0100

    Added environment variables and finished mariadb I replaced the .sql file with a shell script so I can use environment variables. I also added a network bridge to the docker compose. Environment variables were moved from dockerfiles to docker compose.

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -3,6 +3,6 @@ cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=bthomas.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url="$WORDPRESS_SITE_URL" --title="$WORDPRESS_SITE_TITLE" --admin_user="$WORDPRESS_ADM_USER" --admin_password="$WORDPRESS_ADM_PASS" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
 php-fpm7.4 -F
