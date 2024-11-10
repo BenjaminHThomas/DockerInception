ac483f7d566ded189b9a1f988c98b2b632107e12
ben <benjamin.h.thomas1@gmail.com>
2024-11-10 16:10:45 +0100

    start configuring redis with wordpress

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url="$WORDPRESS_SITE_URL" --title="$WORDPRESS_SITE_TITLE" --admin_user="$WORDPRESS_ADM_USER" --admin_password="$WORDPRESS_ADM_PASS" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+
+# Wait for Redis to be ready
+while ! nc -z redis 6379; do
+    echo "Waiting for Redis..."
+    sleep 1
+done
+
+wp core download  
+wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost=mariadb  
+wp config set WP_CACHE true --raw  
+wp config set WP_DEBUG true --raw  
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+
+until wp db check --allow-root do
+    echo "Waiting for database..."
+    sleep 1
+done
+
+wp core install --url="$WORDPRESS_SITE_URL" --title="$WORDPRESS_SITE_TITLE" --admin_user="$WORDPRESS_ADM_USER" --admin_password="$WORDPRESS_ADM_PASS" --admin_email="$WORDPRESS_ADMIN_EMAIL"  
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
+wp user create "${WORDPRESS_USERNAME}" "${WORDPRESS_EMAIL}" --user_pass="${WORDPRESS_PASS}" --role=subscriber  
+chown -R www-data:www-data ./wp-content
+chmod -R 777 ./wp-content && \
+
+
 php-fpm7.4 -F
